29f18eb764cbcc4168b684abfba29247a4203599
Martin Kanestrøm <martinkanestrom95@gmail.com>
2020-02-16 19:43:52 +0100

    Question 1 finished

--- js/question1.js
+++ js/question1.js
@@ -2,15 +2,13 @@ const form = document.querySelector("#contactForm");
 
 form.addEventListener("submit", checkForm);
 
-var checkForm = (event) => {
+function checkForm(event) {
     event.preventDefault();
     let firstName = form.querySelector("#firstName").value.trim();
     if(firstName.length >= 2) {
-        alert(firstName);
+        document.querySelector(".error").style.display = "none";
     }
     else {
         document.querySelector(".error").style.display = "block";
-        
-        
     }
 }
